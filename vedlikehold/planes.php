755546c877351263a64647da669d5c1f8e6bc381
pnitram <martin.pedersen@me.com>
2016-05-30 23:59:11 +0200

    Liten bufix i planes.php

--- vedlikehold/planes.php
+++ vedlikehold/planes.php
@@ -30,7 +30,7 @@ include('./html/admin-start.html');
       <div class="col-xs-12">
         <div class="box">
           <div class="box-header">
-            <h3 class="box-title">Meldinger</h3>
+            <h3 class="box-title">Liste over fly</h3>
           </div>
           <!-- /.box-header -->
           <div class="box-body">
