9ee7aa4bb009d47a3b94fd29ebb4a12be0087e66
pnitram <martin.pedersen@me.com>
2016-06-02 23:52:32 +0200

    La til ruter i meny

---
+++ vedlikehold/FlightRoutes/routes.php
@@ -0,0 +1,119 @@
+<?php 
+$title = "FLY - Admin ";
+
+include('../html/header.html');
+
+include('../html/admin-start.html');
+
+include('../php/Logg.php');
+
+$logg = new Logg();
+
+
+$logg->Ny('Laster FLY side', 'INFO', htmlspecialchars($_SERVER['PHP_SELF']) , 'ikke logget inn');
+
+?>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+      <h1>
+        Vis alle fly
+        <small>Viser en oversikt over alle fly.</small>
+      </h1>
+    <ol class="breadcrumb">
+      <li><a href="../"><i class="fa fa-dashboard"></i> Start</a></li>
+      <li>Fly</li>
+      <!-- Denne lese av script for å sette riktig link aktiv i menyen (husk ID i meny må være lik denne) -->
+      <li class="active">Vis alle fly</li>
+    </ol>
+  </section>
+ <!-- Main content -->
+  <section class="content">
+
+    <!-- Your Page Content Here -->
+
+        <div class="row">
+      <div class="col-xs-12">
+        <div class="box">
+          <div class="box-header">
+            <h3 class="box-title">Liste over fly</h3>
+          </div>
+          <!-- /.box-header -->
+          <div class="box-body">
+            <div id="example2_wrapper" class="dataTables_wrapper form-inline dt-bootstrap">
+              <div class="row">
+                <div class="col-sm-6"></div>
+                <div class="col-sm-6"></div>
+              </div>
+              <div class="row">
+                <div class="col-sm-12">
+                  <table id="example2" class="table table-bordered table-hover dataTable" role="grid" aria-describedby="example2_info">
+                    <thead>
+                      <tr role="row">
+                        <th class="sorting_asc" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-sort="ascending" aria-label="id">ID</th>
+                        <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="FlyNr">Flynr</th>
+                        <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Modell">Flymodell</th>
+                        <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Type">Flytype</th>
+                        <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Plasser">Antall sitteplasser</th>
+                        <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Laget">Årsmodell</th>                      
+                      <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Statuskode Id">Statuskode Id</th></tr>
+                      </tr>
+                    </thead>
+                    <tbody>
+<?php 
+
+include('../php/AdminClasses.php');
+
+$planes = new Planes;
+
+
+print( $planes->ShowAllPlanes() );
+
+
+?> 
+
+
+                    </tbody>
+                    <tfoot>
+                      <tr>
+                        <th rowspan="1" colspan="1">ID</th>
+                        <th rowspan="1" colspan="1">Flynr</th>
+                        <th rowspan="1" colspan="1">Flymodell</th>
+                        <th rowspan="1" colspan="1">Flytype</th>
+                        <th rowspan="1" colspan="1">Antall sitteplasser</th>
+                        <th rowspan="1" colspan="1">Årsmodell</th>
+                        <th rowspan="1" colspan="1">Statuskode Id</th>
+                      </tr>
+                    </tfoot>
+                  </table>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-sm-5">
+                  <div class="dataTables_info" id="example2_info" role="status" aria-live="polite"><?php $rader = new Count; print( $rader->AntallRader('fly') ); ?></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- /.box-body -->
+        </div>
+        <!-- /.box -->
+      </div>
+   
+      <!-- /.col -->
+    </div>
+
+  </section>
+  <!-- /.content -->
+  
+  </div>
+  <!-- /.content-wrapper -->
+  
+
+<?php
+include('../html/admin-slutt.html');
+
+include('../html/script.html');
+
+?>
