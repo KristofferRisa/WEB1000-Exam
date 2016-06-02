a09245bce840dbecc150ef83282c81c31bf9db41
pnitram <martin.pedersen@me.com>
2016-06-02 14:58:06 +0200

    endret base

--- vedlikehold/www/js/validering.js
+++ vedlikehold/www/js/validering.js
@@ -54,6 +54,58 @@ function validerRegistrerFly()
         document.getElementById("melding").innerHTML=feilmelding; 
         return false;
     } 
+}
+
+function validerRegistrerFlyplass()
+{
+  var flyplassNavn=document.getElementById("flyplassNavn").value;
+  var flyplassLAnd=document.getElementById("flyplassLand").value;
+  var flyType=document.getElementById("flyType").value;
+  var flyAntallPlasser=document.getElementById("flyAntallPlasser").value;
+  var flyAarsmodell=document.getElementById("flyAarsmodell").value;
+  var flyStatusKode=document.getElementById("flyStatusKode").value;
+
+  var feilmelding="<div class='alert alert-error'>";
+
+  if (!flyNr)
+    {
+        feilmelding=feilmelding + "Fly nr er ikke korrekt fylt ut.<br />";
+    }
+  if (!flyModell)
+    {
+        feilmelding=feilmelding + "Fly modell er ikke fylt ut. <br />";
+    }
+  if (!flyType)
+    {
+        feilmelding=feilmelding + "Flytype er ikke fylt ut. <br />";
+    }
+   if (!flyAntallPlasser)
+    {
+        feilmelding=feilmelding + "Antall plasser ikke fylt ut. <br />";
+    }
+
+    if (!flyAarsmodell)
+    {
+        feilmelding=feilmelding + "Årsmodell er ikke fylt ut. <br />";
+    }
+
+    if (!flyStatusKode)
+    {
+        feilmelding=feilmelding + "Statuskode mangler. <br />";
+    }
+
+    else {feilmelding=feilmelding + "</div>"}
+
+  if (flyNr && flyModell && flyType && flyAntallPlasser && flyAarsmodell && flyStatusKode)
+    {
+        return true;
+    }
+  else
+    {
+        document.getElementById("melding").style.color="red"; 
+        document.getElementById("melding").innerHTML=feilmelding; 
+        return false;
+    } 
 } 
 
 function clearForm(oForm) {
