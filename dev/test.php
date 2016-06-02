14666a871abaaf581b84c55d16d816df345e2b35
Kristoffer Risa <kristofferrisa@gmail.com>
2016-06-02 11:40:14 +0200

    Fikset require feil

--- dev/test.php
+++ dev/test.php
@@ -18,13 +18,42 @@ $options = [
 $pass_hash = password_hash($pass, PASSWORD_BCRYPT, $options);
 
 echo '<p>ny hash:</p>';
-echo $pass_hash;
+echo $pass_hash.'<br>';
 
-?>
 
-<br>
+if (!function_exists('base_url')) {
+    function base_url($atRoot=FALSE, $atCore=FALSE, $parse=FALSE){
+        if (isset($_SERVER['HTTP_HOST'])) {
+            $http = isset($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) !== 'off' ? 'https' : 'http';
+            $hostname = $_SERVER['HTTP_HOST'];
+            $dir =  str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
 
-<?php
+            $core = preg_split('@/@', str_replace($_SERVER['DOCUMENT_ROOT'], '', realpath(dirname(__FILE__))), NULL, PREG_SPLIT_NO_EMPTY);
+            $core = $core[0];
+
+            $tmplt = $atRoot ? ($atCore ? "%s://%s/%s/" : "%s://%s/") : ($atCore ? "%s://%s/%s/" : "%s://%s%s");
+            $end = $atRoot ? ($atCore ? $core : $hostname) : ($atCore ? $core : $dir);
+            $base_url = sprintf( $tmplt, $http, $hostname, $end );
+        }
+        else $base_url = 'http://localhost/';
+
+        if ($parse) {
+            $base_url = parse_url($base_url);
+            if (isset($base_url['path'])) if ($base_url['path'] == '/') $base_url['path'] = '';
+        }
+
+        return $base_url;
+    }
+}
+echo base_url().'<br>';  
+
+echo base_url(TRUE).'<br>';    //  will produce something like: http://stackoverflow.com/
+echo base_url(TRUE, TRUE).'<br>';
+echo base_url(NULL, TRUE).'<br>';
+
+echo '<p>Paths</p>';
+echo "http://" . $_SERVER['SERVER_NAME'];
+
 
 echo '<p>baner:</p>';
 
@@ -37,3 +66,5 @@ echo  '<p>relative baner</p>';
 echo realpath(dirname(__FILE__)); //
 
 ?>
+
+
--- vedlikehold/php/config.php
+++ vedlikehold/php/config.php
@@ -1,30 +1,12 @@
 <?php
 
     //Usage :
-    // $config = require_once('./config.php');
+    // $config = require(realpath(dirname(__FILE__)).'/config.php');
     // $host = $config['db_host'];
-
-   /* return array('db_host' =>  "127.0.0.1"
-        , 'db_usr' => 'web-is-gr13w'
-        , 'db_pass' => '59091' 
-        , 'db_name' => 'web-is-gr13w'
-        , 'debug' => 'FALSE');
-    */
-
     return array('db_host' =>  "95.85.7.93"
         , 'db_usr' => 'eksamen'
         , 'db_pass' => 'eksamen' 
         , 'db_name' => 'eksamen'
         , 'debug' => 'FALSE'
-        , 'base_url' => 'http://localhost/WEB1000-Exam/vedlikehold/'
-       // , 'base_url' => 'http://localhost/WEB1000-Exam/vedlikehold/'
         );
-
-
-
 ?>
-
-
-<base href="">
-    <base href="https://home.hbv.no/web-is-gr13w/dev/vedlikehold/">
-    https://github.com/joshbuchea/HEAD
