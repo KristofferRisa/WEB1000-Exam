2294bfa9af25329afa2acb3e1897c376098f195d
Kristoffer Risa <kristofferrisa@gmail.com>
2016-06-03 23:28:26 +0200

    Change on deployment

--- vedlikehold/html/start.php
+++ vedlikehold/html/start.php
@@ -1,12 +1,15 @@
 <?php
-      //Husk å endre når man laster opp til skolen
+    //CHANGE ON DEPLOYMENT
     include_once $_SERVER['DOCUMENT_ROOT'] . "/WEB1000-Exam/vedlikehold/php/User.php";
+    // include_once $_SERVER['DOCUMENT_ROOT'] . "/web-is-gr13w/dev/vedlikehold/php/User.php";
     @session_start();
     
     @$innloggetBruker=$_SESSION["brukernavn"];
 
     if (!$innloggetBruker)
     {
+        //CHANGE ON DEPLOYMENT
+        // header('Location: '.$_SERVER['SERVERNAME'].'/web-is-gr13w/dev/vedlikehold/login.php');
         header('Location: '.$_SERVER['SERVERNAME'].'/WEB1000-Exam/vedlikehold/login.php');
         exit;
     }
@@ -14,4 +17,4 @@
     $user = new User();
     $userInfo = $user->GetUsername($_SESSION['brukernavn']);
     
-?>              
+?>
