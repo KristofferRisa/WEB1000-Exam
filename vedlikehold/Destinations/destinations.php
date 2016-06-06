5f5dcb261afb1ca95cdb2b884204b9081f6a9cfc
pnitram <martin.pedersen@me.com>
2016-06-06 21:20:19 +0200

    Destinasjonsfiler

---
+++ vedlikehold/Destinations/destinations.php
@@ -0,0 +1,122 @@
+<?php 
+$title = "FLYAVGANG - Admin ";
+
+include('../html/start.php');
+
+include('../html/header.html');
+
+include('../html/admin-start.html');
+
+
+?>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+      <h1>
+        Vis alle destinasjoner
+        <small>Viser en oversikt over alle destinasjoner.</small>
+      </h1>
+    <ol class="breadcrumb">
+      <li><a href="../"><i class="fa fa-dashboard"></i> Start</a></li>
+      <li>Destinasjoner</li>
+      <!-- Denne lese av script for å sette riktig link aktiv i menyen (husk ID i meny må være lik denne) -->
+      <li class="active">Vis alle destinasjoner</li>
+    </ol>
+  </section>
+ <!-- Main content -->
+  <section class="content">
+
+    <!-- Your Page Content Here -->
+
+        <div class="row">
+      <div class="col-xs-12">
+        <div class="box">
+          <div class="box-header">
+            <h3 class="box-title">Liste over destinasjoner</h3>
+          </div>
+          <!-- /.box-header -->
+          <div class="box-body">
+            <div id="example2_wrapper" class="dataTables_wrapper form-inline dt-bootstrap">
+              <div class="row">
+                <div class="col-sm-6"></div>
+                <div class="col-sm-6"></div>
+              </div>
+              <div class="row">
+                <div class="col-sm-12">
+                  <table id="example2" class="table table-bordered table-hover dataTable" role="grid" aria-describedby="example2_info">
+                    <thead>
+                      <tr role="row">
+                        <th class="sorting_asc" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-sort="ascending" aria-label="DestinasjonsID">Destinasjons ID</th>
+                        <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="FlyplassID">Flyplass ID</th>
+                        <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Navn">Navn</th>
+                        <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Land">Land</th>
+                        <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Landskode">Landskode</th>
+                        <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Stedsnavn">Stedsnavn</th>
+                        <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Geo_lat">Geo_lat</th>
+                        <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Geo_lang">Geo_lang</th>
+                        <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Endret">Endret</th>
+                        <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Handling">Handling</th>            
+                     
+                      </tr>
+                    </thead>
+                    <tbody>
+<?php 
+
+include('../php/AdminClasses.php');
+
+$destination = new Destination;
+
+
+print( $destination->ShowAllDestinations() );
+
+
+?> 
+
+
+                    </tbody>
+                    <tfoot>
+                      <tr>
+                        <th rowspan="1" colspan="1">Destinasjons ID</th>
+                        <th rowspan="1" colspan="1">Flyplass ID</th>
+                        <th rowspan="1" colspan="1">Navn</th>
+                        <th rowspan="1" colspan="1">Land</th>
+                        <th rowspan="1" colspan="1">Landskode</th>
+                        <th rowspan="1" colspan="1">Stedsnavn</th>
+                        <th rowspan="1" colspan="1">Geo_lat</th>
+                        <th rowspan="1" colspan="1">Geo_geo</th>
+                        <th rowspan="1" colspan="1">Endret</th>
+                        <th rowspan="1" colspan="1">Handling</th>
+                        </tr>
+                    </tfoot>
+                  </table>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-sm-5">
+                  <div class="dataTables_info" id="example2_info" role="status" aria-live="polite"><?php $rader = new Count; print( $rader->AntallRader('destinasjon') ); ?></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- /.box-body -->
+        </div>
+        <!-- /.box -->
+      </div>
+   
+      <!-- /.col -->
+    </div>
+
+  </section>
+  <!-- /.content -->
+  
+  </div>
+  <!-- /.content-wrapper -->
+  
+
+<?php
+include('../html/admin-slutt.html');
+
+include('../html/script.html');
+
+?>
---
+++ vedlikehold/Destinations/destinationsAdd.php
@@ -0,0 +1,115 @@
+<?php 
+$title = "FLY - Admin";
+
+include('../html/start.php');
+
+include('../html/header.html');
+
+include('../html/admin-start.html');
+
+// Validering og innsending av skjemadata
+include('../php/addPlaneFormInput.php');
+
+?>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+
+      <h1>
+        Registrere nytt fly
+        <small>Her kan du registrere nye fly i databasen</small>
+      </h1>
+    <ol class="breadcrumb">
+      <li><a href="../"><i class="fa fa-dashboard"></i> Start</a></li>
+      <li>Fly og seteoversikt</li>
+      <!-- Denne lese av script for å sette riktig link aktiv i menyen (husk ID i meny må være lik denne) -->
+      <li class="active">Legg til fly</li>
+    </ol>
+  </section>
+ <!-- Main content -->
+  <section class="content">
+
+
+    <!-- Your Page Content Here -->
+
+        <div class="row">
+      <div class="col-sm-12">   
+                 <!-- Horizontal Form -->
+          <div class="box box-info">
+            <div class="box-header with-border"><?php echo $errorMelding; ?><div id="melding"></div>
+           <h3 class="box-title">Skjema</h3>
+            </div>
+            <!-- /.box-header -->
+
+
+
+            <!-- form start -->
+
+            <form method="post" class="form-horizontal" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" onsubmit="return validerRegistrerFly()">
+              <div class="box-body">        
+
+                <div class="form-group">
+                  <label for="flyNr" class="col-sm-2 control-label">Flynr</label>
+                  <div class="col-sm-10">
+                    <input type="text" class="form-control" id="flyNr" name="flyNr" placeholder="Flynr" value="<?php echo @$_POST['flyNr'] ?>">
+                  </div>
+                </div>
+
+                <div class="form-group">
+                  <label for="flyModell" class="col-sm-2 control-label">Flymodell</label>
+                  <div class="col-sm-10">
+                    <input type="text" class="form-control" id="flyModell" name="flyModell" placeholder="Flymodell" value="<?php echo @$_POST['flyModell'] ?>">
+                  </div>
+                </div>
+
+                <div class="form-group">
+                  <label for="flyType" class="col-sm-2 control-label">Flytype</label>
+                  <div class="col-sm-10">
+                    <input type="text" class="form-control" id="flyType" name="flyType" placeholder="Flytype" value="<?php echo @$_POST['flyType'] ?>">
+                  </div>
+                </div>
+
+                <div class="form-group">
+                  <label for="flyAntallPlasser" class="col-sm-2 control-label">Antall sitteplasser</label>
+                  <div class="col-sm-10">
+                    <input type="text" class="form-control" id="flyAntallPlasser" name="flyAntallPlasser" placeholder="Antall sitteplasser" value="<?php echo @$_POST['flyAntallPlasser'] ?>">
+                     </div>
+                </div>
+
+                <div class="form-group">
+                  <label for="flyLaget" class="col-sm-2 control-label">Årsmodell</label>
+                  <div class="col-sm-10">
+                    <input type="text" class="form-control" id="flyAarsmodell" name="flyAarsmodell" placeholder="yyyy" value="<?php echo @$_POST['flyAarsmodell'] ?>">
+                  </div>
+                </div>
+
+              <!-- /.box-body -->
+              <div class="box-footer">
+                <button class="btn btn-default" onclick="fjernMelding();clearForm(this.form);return false;">Nullstill</button>
+
+                <button type="submit" class="btn btn-info pull-right">Legg til</button>
+              </div>
+              <!-- /.box-footer -->
+            </form>
+          </div>
+          <!-- /.box -->
+
+      </div>
+   
+      <!-- /.col -->
+    </div>
+
+  </section>
+  <!-- /.content -->
+  
+  </div>
+  <!-- /.content-wrapper -->
+  
+
+<?php
+include('../html/admin-slutt.html');
+
+include('../html/script.html');
+
+?>
