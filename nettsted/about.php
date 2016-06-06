21e921dea8ae15e0ba58683fb1f3003094fb984c
unnilehhn <unnilehhn@gmail.com>
2016-06-06 14:16:04 +0200

    Laget om side med linker til rammeverk og bibliotek

---
+++ nettsted/about.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="no">
+<head>
+  <title>About</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="www/css/adminLTE.min.css">
+  <link rel="stylesheet" type="text/css" href="www/css/style.css">
+  <style>
+    
+    .links
+    {
+      padding-left:100px;
+    }
+    
+  </style>
+</head>
+<body>
+  
+  <?php
+    include ("./html/nav.html");
+  ?>
+  
+   <div class="jumbotron text-center">
+    <div class="login-logo">
+     <b>B</b>JARUM <b>A</b>IRLINES  
+    </div>
+  </div>
+  
+  
+<div class="container-fluid">
+  <div class="row content">
+    <div class="col-sm-4 sidenav">
+      <h1> GRUPPE 13 </h1>
+      <br>
+      <p> Dette er et nettsted laget i forbindelse med eksamen i WEB 1000 våren 2016 ved Høgskolen i Sørøst-Norge. Nettstedet er laget av Martin Pedersen, Kristoffer Risa og Unni Le Hoai Huong Ngoc </p>
+      <br> <br>
+      <p> Lenkene til høyre henviser til bibliotekene og rammeverkene vi har brukt for å lage denne nettsiden. </p>
+    </div>
+    
+          <div class="col-sm-8 links">
+        <form class="form-inline" role="form">
+          <div class="form-group">
+                  <h1> BIBLIOTEKER OG RAMMEVERK </h1>
+                  <br>
+                  <ul>
+                    <li><a href="https://almsaeedstudio.com/AdminLTE">Almsaeed Studie, AdminLTE</a></li>
+                    <li><a href="http://getbootstrap.com/">Bootstrap</a></li>
+                    <li><a href="http://semantic-ui.com/">Semantic UI</a></li>
+                    <li><a href="https://jquery.com/">jQuery</a></li>
+                  </ul>
+           </div>
+   </div>
+   
+
+</div>
+
+  <?php
+  include ("./html/footer.html");
+  ?>
+
+</body>
+
+</html>
+    
